8e9b3a11daac97fc57583bcf6ad99fc0bd643821
toshakparmar <toshakparmar2000@gmail.com>
2026-05-21 17:36:48 +0530

    --Chore: Update the Build issues (warnings)

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,6 +1,6 @@
-import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
+// import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
+// import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
+// import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import PageMeta from "../../components/common/PageMeta";
 
 export default function Home() {
@@ -10,8 +10,9 @@ export default function Home() {
         title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
+      {/* <div className="grid grid-cols-12 gap-4 md:gap-6"> */}
+      <h3 className="text-gray-700 text-center text-3xl font-bold">WELCOME TO THE ORANGE GLOBAL ADMIN DASHBOARD</h3>
+      {/* <div className="col-span-12 space-y-6 xl:col-span-7">
           <EcommerceMetrics />
 
           <MonthlySalesChart />
@@ -19,20 +20,20 @@ export default function Home() {
 
         <div className="col-span-12 xl:col-span-5">
           <MonthlyTarget />
-        </div>
+        </div> */}
 
-        {/* <div className="col-span-12">
+      {/* <div className="col-span-12">
           <StatisticsChart />
         </div> */}
 
-        {/* <div className="col-span-12 xl:col-span-5">
+      {/* <div className="col-span-12 xl:col-span-5">
           <DemographicCard />
         </div>
 
         <div className="col-span-12 xl:col-span-7">
           <RecentOrders />
         </div> */}
-      </div>
+      {/* </div> */}
     </>
   );
 }
